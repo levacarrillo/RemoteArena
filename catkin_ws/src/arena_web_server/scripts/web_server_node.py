0a1b8425a5969d04a63bfce7d7df3c5765fd5e8f
Luis Nava <levacarrillo@gmail>
2024-08-20 20:20:07 -0600

    Update web_server

--- catkin_ws/src/arena_web_server/scripts/web_server_node.py
+++ catkin_ws/src/arena_web_server/scripts/web_server_node.py
@@ -1,20 +1,62 @@
 #!/usr/bin/env python3
 import rospy
-from flask import Flask, render_template, jsonify
+import os
+from flask import Flask, render_template, request, jsonify
 from std_msgs.msg import String
 
 app = Flask(__name__, static_folder = '../templates/static', template_folder = '../templates')
 
+
 @app.route('/viewer')
+def viewer():
+    return render_template('viewer.html')
+
+@app.route('/home')
 def home():
+    return render_template('home.html')
+
+@app.route('/index')
+def index():
     return render_template('index.html')
 
-@app.route('/executed_command/<command>', methods=['POST'])
-def executed_command(command):
+@app.route('/robot_control_command/<command>', methods=['POST'])
+def robot_control_command(command):
     print('ros-command:' + command)
     if not rospy.is_shutdown():
         pub.publish(command)
     return jsonify(message='Command->' + command)
+
+@app.route('/light_control_command/<id>/<status>', methods=['POST'])
+def light_control_command(id, status):
+    print('lights: ' + id + ' status: ' + status)
+    #if not rospy.is_shutdown():
+    #    pub.publish(command)
+    return jsonify(message='Command->')
+
+@app.route('/upload', methods=['POST'])
+def upload_file():
+
+    if 'file' not in request.files:
+        print('No file part')
+        return 'No file part'
+
+    file = request.files['file']
+
+
+    if file.filename == '':
+        print('No selecte file')
+        return 'No selected file'
+    
+    if file: 
+        user_name = os.getlogin()
+        abspath = f'/home/{user_name}/ProgramsFile'
+        if not os.path.exists(abspath):
+            os.makedirs(abspath)
+        print('Saving file->', file.filename, 'at->', abspath)
+        file_path = os.path.join(abspath, file.filename)
+        file.save(file_path)
+
+    return 'file successfully added'
 
 def start_ros_node():
     global pub
--- catkin_ws/src/arena_web_server/templates/static/script.js
+++ catkin_ws/src/arena_web_server/templates/static/script.js
@@ -1,12 +1,46 @@
-function sendCommand(command) {
-    console.log('Command:', command);
-    fetch(`/executed_command/${command}`, {
+function controlCommand(command) {
+    console.log('command:', command);
+    fetch(`/robot_control_command/${command}`, {
         method: 'POST',
     }).then(response => response.json())
     .then(data => {
         console.log(data);
     });
 }
-function handleClick(cb) {
+function handleChecked(cb) {
     console.log(cb.id,cb.checked);
+    const id = cb.id;
+    const status = cb.checked;
+    fetch(`/light_control_command/${id}/${status}`, {
+        method: 'POST',
+    }).then(response => response.json())
+    .then(data => {
+        console.log(data);
+    });
+}
+async function uploadFile() {
+    const form = document.getElementById('uploadForm');
+    const formData = new FormData(form);
+    console.log(formData);
+
+    try {
+        const response = await fetch('/upload', {
+            method: 'POST',
+            body: formData
+        })
+        const result = await response.text()
+        console.log(result);
+        console.log(response);
+    } catch (error) {
+        console.log('error: ' + error);
+    }
+}
+function fileUploaderChanged() {
+    const inputUploader = document.getElementById('inputUploader');
+    const uploadButton = document.getElementById('uploadButton');
+    if (inputUploader.files.length > 0) {
+        uploadButton.disabled = true;
+    }
+    console.log(uploadButton)
+    console.log(inputUploader.files)
 }
