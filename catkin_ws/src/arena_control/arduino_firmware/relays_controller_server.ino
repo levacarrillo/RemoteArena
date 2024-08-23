ea65132d74521efe6328b61e49d7d51959ef301e
Luis Nava <levacarrillo@gmail>
2024-08-23 02:03:12 -0600

    Update relays_controller_server.ino

--- catkin_ws/src/arena_control/arduino_firmware/relays_controller_server.ino
+++ catkin_ws/src/arena_control/arduino_firmware/relays_controller_server.ino
@@ -4,25 +4,44 @@
 // OBJECT NODE
 ros::NodeHandle nh;
 
-// ARDUINO PINES
-const int light_pins[2] = {2, 3};
+// ARDUINO'S PINES
+const int light_pins[2]   = {2, 3};
+const int charger_pins[2] = {4, 5};
 
-
-bool callback(arena_control::control_lights::Request & req, arena_control::control_lights::Response & res) {
+bool light_callback(arena_control::control_lights::Request &req, arena_control::control_lights::Response &res) {
   nh.loginfo("-------------------");
-  nh.loginfo(decltype(1));
-  nh.loginfo(decltype(req.light_states[0]));
-  digitalWrite(light_pins[0], req.light_states[0] ? HIGH : LOW);
-  digitalWrite(light_pins[1], req.light_states[1] ? HIGH : LOW);
+  char buffer[20];
+  sprintf(buffer, "Turn on/off lights: %d", (int)req.light_states[0]);
+  nh.loginfo(buffer);
+  sprintf(buffer, "Turn on/off lights: %d", (int)req.light_states[1]);
+  nh.loginfo(buffer);
+
+  for(int i=0; i<sizeof(light_pins); i++) {
+    if((int)req.light_states[i] == 1) {
+      digitalWrite(light_pins[i], LOW);
+    } else {
+      digitalWrite(light_pins[i], HIGH);
+    }
+  }
   return true;
 }
 
 
-ros::ServiceServer<arena_control::control_lights::Request, arena_control::control_lights::Response> service("lights_control", &callback);
+ros::ServiceServer<arena_control::control_lights::Request, arena_control::control_lights::Response> light_server("lights_control", &light_callback);
 
 void setup() {
   nh.initNode();
-  nh.advertiseService(service);
+  nh.advertiseService(light_server);
+  
+  // SETTING PINS MODE
+  for(int i=0; i<sizeof(light_pins); i++) {
+    pinMode(light_pins[i], OUTPUT);
+    digitalWrite(light_pins[i], HIGH);
+  }
+  for(int i=0; i<sizeof(charger_pins); i++) {
+    pinMode(charger_pins[i], OUTPUT);
+    digitalWrite(charger_pins[i], HIGH);
+  }
 }
 
 void loop() {
