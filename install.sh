f145d0cd121c20593217af99592d5570a277cad9
luisnava <luisnava@luisnava.myguest.virtualbox.org>
2024-09-24 20:58:50 -0700

    Add install.sh

---
+++ install.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+# Script of basic instalation
+
+sudo apt-get install ros-noetic-web-video-server -y
+sudo apt-get install ros-noetic-usb-cam -y
+sudo apt-get install ros-noetic-video-stream-opencv -y
+sudo apt-get install ros-noetic-roslint -y
+sudo apt-get install ros-noetic-rviz-visual-tools -y
+sudo apt-get install python3-pip -y
+sudo apt-get install python3-venv -y
+sudo pip3 install flask
+sudo pip3 install rospkg
+sudo apt-get install ros-noetic-rosserial-python -y
+sudo apt-get install ros-noetic-rosserial-arduino -y
+sudo apt-get install postgresql -y
+sudo pip3 install psycopg2-binary
+sudo pip3 install flask-sqlalchemy
+sudo pip3 install python-dotenv
