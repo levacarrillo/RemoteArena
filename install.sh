85a6118bdf6ae84382fb4d77661860286a9cf688
Luis Nava <levacarrillo@gmail.com>
2024-10-24 22:55:31 -0600

    Update install.sh

--- install.sh
+++ install.sh
@@ -3,13 +3,20 @@
 
 sudo apt-get update -y 
 sudo apt-get upgrade -y
+sudo apt-get install ros-noetic-cv-bridge -y
+sudo apt-get install ros-noetic-image-transport -y
+sudo apt-get install ros-noetic-tf -y
+sudo apt-get install ros-noetic-tf2-tools -y
+sudo apt-get install ros-noetic-tf2-geometry-msgs -y
+sudo apt-get install ros-noetic-rosserial-python -y
+
 sudo apt-get install ros-noetic-web-video-server -y
 sudo apt-get install ros-noetic-usb-cam -y
 sudo apt-get install ros-noetic-video-stream-opencv -y
 sudo apt-get install ros-noetic-roslint -y
 sudo apt-get install ros-noetic-rviz-visual-tools -y
 sudo apt-get install ros-noetic-map-server -y
-sudo apt-get install ros-noetic-arudo-ros -y
+sudo apt-get install ros-noetic-aruco-ros -y
 sudo apt-get install ros-noetic-plotjuggler-ros -y
 
 sudo apt-get install python3-pip -y
@@ -22,3 +29,4 @@ sudo apt-get install postgresql -y
 sudo pip3 install psycopg2-binary
 sudo pip3 install flask-sqlalchemy
 sudo pip3 install python-dotenv
+sudo pip3 install flask-cors
