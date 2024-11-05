9c783d7857397f333ec43f60b01669addbae9150
Luis Nava <levacarrillo@gmail.com>
2024-11-05 12:09:30 -0600

    Fix relays_controller firmware

--- catkin_ws/src/arena_control/arduino_firmware/relays_controller_server.ino
+++ catkin_ws/src/arena_control/arduino_firmware/relays_controller_server.ino
@@ -12,30 +12,34 @@ bool light_bulb_status[2]= {false, false};
 
 // RESETTING LIGHTS
 void reset_lights() {
+  // TURNING ON LIGHTS
   for (int i=0; i<sizeof(light_pin); i++){
-    // TURNING ON LIGHTS
-    digitalWrite(light_pin[i], HIGH);
-    delay(800);
-    // TURNING OFF LIGHTS AGAIN
     digitalWrite(light_pin[i], LOW);
+  }
+  delay(600);
+  // TURNING OFF LIGHTS AGAIN
+  for (int i=0; i<sizeof(light_pin); i++){
+    digitalWrite(light_pin[i], HIGH);
   }
 }
 
 // RESETTING CHARGERS
 void reset_chargers() {
+  // TURNING OFF CHARGERS
   for (int i=0; i<sizeof(charger_pin); i++){
-    // TURNING OFF CHARGERS
     digitalWrite(charger_pin[i], LOW);
-    delay(800);
-    // TURNING ON CHARGERS AGAIN
+  }
+  delay(600);
+  // TURNING ON CHARGERS AGAIN
+  for (int i=0; i<sizeof(charger_pin); i++){
     digitalWrite(charger_pin[i], HIGH);
   }
 }
 
 bool getParams() {
   bool params_loaded = true;
-  if(!nh.getParam("/arena_hardware/light_bulbs", light_bulb_status, 2)) { params_loaded = false; }
-  if(!nh.getParam("/arena_hardware/chargers", charger_status, 2)) { params_loaded = false; }
+  if(!nh.getParam("/light_bulbs", light_bulb_status, 2)) { params_loaded = false; }
+  if(!nh.getParam("/chargers", charger_status, 2)) { params_loaded = false; }
 
   return params_loaded;
 }
@@ -58,8 +62,8 @@ void loop() {
     nh.logwarn("Arena's control params not loaded correctly");
   } else {
     for(int i=0; i<2; i++) {
-      digitalWrite(light_pin[i], light_bulb_status[i]);
-      digitalWrite(charger_pin[i], light_bulb_status[i]);
+      digitalWrite(light_pin[i], !light_bulb_status[i]);
+      digitalWrite(charger_pin[i], charger_status[i]);
     }
   }
 
