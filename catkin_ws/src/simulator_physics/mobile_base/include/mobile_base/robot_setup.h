84b9d48327ddec4fcca5d57a2fab8af32109ed4a
Luis Nava <levacarrillo@gmail.com>
2024-11-24 02:14:01 -0600

    Update rosparam's names

--- catkin_ws/src/simulator_physics/mobile_base/include/mobile_base/robot_setup.h
+++ catkin_ws/src/simulator_physics/mobile_base/include/mobile_base/robot_setup.h
@@ -3,13 +3,13 @@ float robot_pose_y;
 float robot_pose_w;
 
 bool setInitialPose() {
-    if(ros::param::has("robot_pose_x")) ros::param::get("robot_pose_x", robot_pose_x);
+    if(ros::param::has("/robot_pose_x")) ros::param::get("robot_pose_x", robot_pose_x);
     else { ROS_ERROR("There's no parameter for robot_pose_x"); return false; }
 
-    if(ros::param::has("robot_pose_y")) ros::param::get("robot_pose_y", robot_pose_y);
+    if(ros::param::has("/robot_pose_y")) ros::param::get("robot_pose_y", robot_pose_y);
     else { ROS_ERROR("There's no parameter for robot_pose_y"); return false; }
 
-    if(ros::param::has("robot_pose_w")) ros::param::get("robot_pose_w", robot_pose_w);
+    if(ros::param::has("/robot_pose_w")) ros::param::get("robot_pose_w", robot_pose_w);
     else { ROS_ERROR("There's no parameter for robot_pose_w"); return false; }
 
     return true;
